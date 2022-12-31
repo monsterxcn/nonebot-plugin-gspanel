23e24549c8d63fd8f7ca7de6e4f4ff5e27fdf465
monsterxcn <monsterxcn@gmail.com>
2022-12-31 20:13:40 +0800

    :bug: 修复小程序接口请求错误

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-gspanel",
-    version="0.2.9",
+    version="0.2.10",
     author="monsterxcn",
     author_email="monsterxcn@gmail.com",
     description="Genshin player cards plugin for NoneBot2",
